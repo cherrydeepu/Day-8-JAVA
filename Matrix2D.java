9f9abb92c64d473f731d7ad243c62c6715d5f22c
Rishitha <trishithapandu2218@gmail.com>
2024-12-09 20:25:56 +0530

    Add files via upload

---
+++ Matrix2D.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Matrix2D{
+    public static void main(String args[]){
+        //initialization of 2D Array
+        int a[][]={{1,2},{3,4}};
+        //Accessing 1D array in 2D Array
+        System.out.println(Arrays.toString(a[0]));
+        System.out.println(Arrays.toString(a[1]));
+    }
+}
